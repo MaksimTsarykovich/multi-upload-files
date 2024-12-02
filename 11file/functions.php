d07d42fe6c24c4b5f30ad402d459df23b756b0fd
MaksimTsarykovich <maksimtsarykovich@gmail.com>
2024-12-02 16:32:19 +0300

    Add files via upload

--- 11file/functions.php
+++ 11file/functions.php
@@ -1,15 +1,59 @@
 <?php
 
+use JetBrains\PhpStorm\NoReturn;
+
 require_once('config.php');
 
-function getAllImages($mysqli)
+function getAllImages($mysqli): array
 {
     $sql = "SELECT * FROM images";
     $result = mysqli_query($mysqli, $sql);
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function returnToHome()
+function isImageValid($fileSize, $fileType): bool
+{
+    if (empty($fileSize)) {
+        $_SESSION['error'] = "Выберете изображения";
+        return false;
+    }
+    if (!($fileType == "image/jpeg" || $fileType == "image/png")) {
+        $_SESSION['error'] = "Можно загрузить изображение только в формате .jpg и .png";
+        return false;
+    }
+    return true;
+}
+
+function saveImage($fileName, $fileTmpName, $mysqli): bool
+{
+    $upLoadDir = __DIR__ . '/uploads/';
+    $upLoadFile = $upLoadDir . basename($fileName);
+    return move_uploaded_file($fileTmpName, $upLoadFile);
+}
+
+function addToDatabase($fileName, $mysqli): bool
+{
+    $sql = "INSERT INTO `images` (`id`, `filename`) VALUES (NULL, '{$fileName}')";
+    return mysqli_query($mysqli, $sql);
+}
+
+function createUnicName($nameLength, $fileType): string
+{
+    $randomName = "";
+    for ($i = 0; $i < $nameLength; $i++) {
+        $codepointUpperCase = chr(random_int(65, 90));
+        $codepointLowerCase = chr(random_int(97, 122));
+        $number = random_int(0, 100);
+        $randomName .= $codepointUpperCase . $codepointLowerCase . $number;
+    }
+    if ($fileType == "image/jpeg") {
+        return $randomName . ".jpg";
+    } else {
+        return $randomName . ".png";
+    }
+}
+
+#[NoReturn] function returnToHome(): void
 {
     header("Location: /Task/11file/index.php");
     exit();
--- 11file/upload.php
+++ 11file/upload.php
@@ -7,43 +7,22 @@ if (!($_SERVER["REQUEST_METHOD"] == "POST")) {
     returnToHome();
 }
 
-foreach ($_FILES["files"]["type"] as $type) {
-    if (!($type == "image/jpeg" || $type == "image/png")) {
-        $_SESSION['error'] = "Можно загрузить изображение только в формате .jpg и .png";
-        returnToHome();
+foreach ($_FILES["files"]["name"] as $key => $image) {
+
+    $fileTmpName = $_FILES["files"]["tmp_name"][$key];
+    $fileSize = $_FILES["files"]["size"][$key];
+    $fileType = $_FILES["files"]["type"][$key];
+
+    !isImageValid($fileSize, $fileType) ? returnToHome() :
+
+    $fileName = createUnicName(2,$fileType);
+
+    if (!saveImage($fileName, $fileTmpName, $mysqli)) {
+        $_SESSION['error'] = "Ошибка при загрузки изображения";
+    }
+    if (!addToDatabase($fileName, $mysqli)){
+        $_SESSION['error'] = "Ошибка записи в базу данных";
     }
 }
-
-if (!(empty($_FILES["files"]["size"]))) {
-    $upLoadDir = __DIR__ . '\uploads\\';
-    $upLoadFile = [];
-    foreach ($_FILES["files"]['name'] as $key => $file) {
-        $upLoadFile[$key] = $upLoadDir . basename($file);
-    }
-    foreach ($_FILES["files"]["tmp_name"] as $key => $tmp_name) {
-        move_uploaded_file($tmp_name, $upLoadFile[$key]);
-    }
-
-} else {
-    $_SESSION['error'] = "Выберете изображения";
-    returnToHome();
-}
-
-foreach ($_FILES["files"]["name"] as $fileName) {
-    print_r($fileName);
-    $sql = "INSERT INTO `images` (`id`, `filename`) VALUES (NULL, '{$fileName}')";
-    if (!(mysqli_query($mysqli, $sql))) {
-        $_SESSION['error'] = "Ошибка при записи в бд: ".mysqli_error($mysqli);
-        returnToHome();
-    }
-}
-
 $_SESSION['success'] = "Изображения успешно загружены";
-
 returnToHome();
-
-
-
-
-
-
